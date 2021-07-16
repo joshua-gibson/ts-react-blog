adbf5f6b12ed4ed3f22592e74b50677dca4f4ea8
joshua-gibson <joshua.gibson.dev@gmail.com>
2021-07-16 14:10:58 +0100

    editor and display

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,53 @@
-import React, {useCallback} from 'react';
-import PostEditor from './PostEditor';
+import React, {useCallback, useState} from 'react';
 import Navbar from './Navbar';
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState } from 'draft-js';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { convertToHTML } from 'draft-convert';
+import DOMPurify from 'dompurify';
 
 
-function App() {
+const App = () => {
+  const [editorState, setEditorState] = useState(
+    () => EditorState.createEmpty(),
+  );
+  const  [convertedContent, setConvertedContent] = useState(null);
+
+  const handleEditorChange = (state) => {
+    setEditorState(state);
+    convertContentToHTML();
+  }
+  const convertContentToHTML = () => {
+    let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(currentContentAsHTML);
+  }
+
+  const createMarkup = (html) => {
+    return  {
+      __html: DOMPurify.sanitize(html)
+    }
+  }
+
   const onListClick = useCallback((item: string) => {alert(item)}, [])
+
+
   return (
     <div>
       <Navbar items={['item1','item2']} onClick={onListClick}/>
 App Component
-<PostEditor/>
-    </div>
+<div style={{ border: "1px solid black", padding: '2px', minHeight: '400px' }}>
+        <Editor
+          editorState={editorState}
+          onEditorStateChange={handleEditorChange}
+          wrapperClassName="wrapper-class"
+        editorClassName="editor-class"
+        toolbarClassName="toolbar-class"
+        />
+      </div>
+      <div className="preview" dangerouslySetInnerHTML={createMarkup(convertedContent)}></div>
+
+      </div>
+
   );
 }
 
--- src/components/PostEditor.tsx
+++ src/components/PostEditor.tsx
@@ -1,23 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Editor } from "react-draft-wysiwyg";
-import { EditorState } from "draft-js";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 
-const PostEditor = () => {
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
-  useEffect(() => {
-    console.log(editorState);
-  }, [editorState]);
+const PostEditor = (editorState, onEditorStateChange) => {
+
   return (
     <div>
       <h1>Post Editor</h1>
       <div style={{ border: "1px solid black", padding: '2px', minHeight: '400px' }}>
         <Editor
           editorState={editorState}
-          onEditorStateChange={setEditorState}
+          onEditorStateChange={onEditorStateChange}
         />
       </div>
     </div>
